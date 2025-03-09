e17d56a1d9d40e448a075f26194fe08ad34866ae
Ike Gabriel Yuson <iggyyuson09@gmail.com>
2025-03-10 00:28:17 +0800

    Initial commit

---
+++ test/appsync-events.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppsyncEvents from '../lib/appsync-events-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-events-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncEvents.AppsyncEventsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
